6e2152c7c6999f8eac20782ecfb32ddef2587344
rokadeamol25 <rokadeamol25@gmail.com>
2025-03-20 07:07:33 +0530

    server,js

--- server.js
+++ server.js
@@ -54,4 +54,11 @@ app.post('/api/students', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
